0b8cea5e2c93ef1423a06c70e91e69fc31051ab2
parimalnaik <parimalnaik@gmail.com>
2025-06-23 12:50:20 -0400

    Google Auth

---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,15 @@
+import { Header } from "components"
+
+const AllUsers = () => {
+  return (
+    <main className="dashboard wrapper">
+      <Header 
+        title={`Trips page`}
+        description="All User details"
+      />
+      All users page content
+    </main>
+  )
+}
+
+export default AllUsers;
